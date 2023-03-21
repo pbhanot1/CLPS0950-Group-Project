4e04fd6a0ef7d161d6a19d9fb4c65a2bdd48c99a
mirawhite <mirawhite@macbook-pro-383.devices.brown.edu>
2023-03-21 18:50:41 -0400

    Commit

--- Face1.m
+++ Face1.m
@@ -1,1 +1,15 @@
-%face1
+%Face 1 (input)
+%written on 3/15/23
+%by Priya Bhanot
+%this script creates a basic face image
+
+face1 = ones(10);
+%eyes
+face1(2:3, 3:4) = zeros;
+face1(2:3, 7:8) = zeros;
+%nose
+face1(5:6,5:6) = zeros;
+%mouth
+face1(8:9, 3:8) = zeros;
+
+disp(face1)
